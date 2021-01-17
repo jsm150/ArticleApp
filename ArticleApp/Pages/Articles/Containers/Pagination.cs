a3536382b362c1265e34d694e04a2ff32b0b9198
수민 정 <drt00784@naver.com>
2021-01-17 22:30:40 +0900

    페이징 구현

---
+++ ArticleApp/Pages/Articles/Containers/Pagination.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace ArticleApp.Pages.Articles.Containers
+{
+    public class Pagination
+    {
+        public int PageIndex { get; set; } = 0;
+        public int PageSize { get; set; } = 10;
+        public int ButtonSize { get; set; } = 3;
+        public int ButtonFirstIdx => (PageIndex / ButtonSize) * ButtonSize;
+        public int ButtonLastIdx => ButtonFirstIdx + ButtonSize - 1;
+        public int ButtonEndIdx => (int)Math.Ceiling(DataCount / (double)PageSize) - 1;
+        public int DataCount { get; set; }
+        public Func<Task> OnChanged { get; set; }
+
+        public void OnClick(int idx)
+        {
+            PageIndex = idx > ButtonEndIdx ? ButtonEndIdx : idx;
+            OnChanged?.Invoke();
+        }
+    }
+}
